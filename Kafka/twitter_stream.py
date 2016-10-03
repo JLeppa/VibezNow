cab6d94491370c0343b6f76cb53a38de4683ee8e
Ubuntu <ubuntu@ip-172-31-0-234.us-west-2.compute.internal>
2016-10-03 00:19:14 +0000

    cleaned kafka producer

--- Kafka/twitter_stream.py
+++ Kafka/twitter_stream.py
@@ -4,8 +4,6 @@ import simplejson
 import requests
 import time
 from requests_oauthlib import OAuth1
-#from kafka.client import SimpleClient
-#from kafka.producer import KeyedProducer
 from kafka import KafkaProducer
 
 def main():
@@ -16,7 +14,7 @@ def main():
     if len(sys.argv) != 1 and len(sys.argv) != 3:
         print "Usage:"
         print "For real-time stream: ./twitter_stream"
-        print "For reading from file: ./twitter_stream file_name messages_per_second"
+        print "For reading from file: ./twitter_stream file_name wait_time_between_messages"
         quit()
 
     # Set up Kafka producer
@@ -75,62 +73,26 @@ def main():
     
     # Read tweets from a file and produce them
     if len(sys.argv) == 3:
-        for x in range(30):
+        # Set how many times file is looped over
+        for x in range(1):
             with open(sys.argv[1]) as fh:
                 msg_cnt = 0
                 for line in fh:
-                #print line
                     tweet = line
                     # Produce all messages to Kafka topic "twitter_stream"
-                    #producer_all.send('twitter_stream', tweet)
-                # Produce messages that include word from the topic list to separate Kafka topics
-                #json_tweet = simplejson.loads(tweet)
-                #if 'text' in json_tweet:
-                #    message = json_tweet['text'].encode("utf-8","replace")
-                #    for word in topic_words:
-                #        if word in message:
-                #            #print message
-                #            #print topic_words[word]
-                #            producer_topic.send(topic_words[word], tweet)
-                #msg_cnt += 1
-                #if msg_cnt % 100 == 0:
-                    #print msg_cnt
-                #wait = raw_input("PRESS ENTER TO CONTINUE.")
-                #quit()
-            #print msg_cnt
+                    producer_all.send('twitter_stream', tweet)
+                    # Produce messages that include word from the topic list to separate Kafka topics
+                    json_tweet = simplejson.loads(tweet)
+                    if 'text' in json_tweet:
+                        message = json_tweet['text'].encode("utf-8","replace")
+                        for word in topic_words:
+                            if word in message:
+                                producer_topic.send(topic_words[word], tweet)
+                    msg_cnt += 1
+                    if msg_cnt % 100 == 0:
+                        print msg_cnt
+                    #wait = raw_input("PRESS ENTER TO CONTINUE.")
+                    time.sleep(float(sys.argv[2]))
 
 if __name__ == "__main__":
     main()
-
-"""
-class Producer(object):
-
-    def __init__(self, addr):
-        self.client = SimpleClient(addr)
-        self.producer = KeyedProducer(self.client)
-
-    def produce_msgs(self, src_partition):
-        msg_cnt = 0
-        for tweet in stream.iter_lines():
-            if not tweet:
-                continue
-            else:
-                try:
-                    self.producer.send_messages('twitter_test', src_partition, tweet)
-                    msg_cnt += 1
-                except ChunkedEncodingError:
-                    continue
-            if scenario == 2:
-                if msg_cnt % 100 == 0:
-                    print msg_cnt
-                #time.sleep(0.05)
-                #if msg_cnt >= 200000:
-                #    break
-
-if __name__ == "__main__":
-    args = sys.argv
-    ip_addr = str(args[1])
-    partition_key = str(args[2])
-    prod = Producer(ip_addr)
-    prod.produce_msgs(partition_key)
-"""
