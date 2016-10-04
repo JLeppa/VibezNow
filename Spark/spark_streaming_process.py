6a29f2668aaf6e726fdd28c0f959f77664e0d90f
Ubuntu <ubuntu@ip-172-31-0-235.us-west-2.compute.internal>
2016-10-04 09:21:22 +0000

    cleaned script until ntf values calculated

--- Spark/spark_streaming_process.py
+++ Spark/spark_streaming_process.py
@@ -51,8 +51,50 @@ def messages_to_words(tweets):
     # Stem the words
     words = words.map(lambda x: stem(x))
 
-    # Return the words
-    return words
+    # Filter words not in the set of words used for bag-of-words
+    words = words.filter(lambda x: x in word_set_bc.value)
+
+    # Count the number of words
+    words = words.map(lambda word: (word, 1))
+    word_count = words.reduceByKey(lambda x, y: x + y)
+
+    # Return the words and their counts
+    return word_count
+
+def word_count_to_ntf(tf_rdd):
+    """ Input is an RDD containing list of tuples in (term, term_freq) format
+    Output is an RDD containing the same list as (term, a+(1-a)*term_freq/max(term_freq))"""
+
+    nf = 0.4 # Normalization factor used when calculating normalized term frequency
+
+    # Search for the max term frequency, max_tf, from all term frequencies, tf
+    freq = tf_rdd.map(lambda x: x[1])
+    max_tf = freq.max()
+
+    # Calculate the ntf
+    ntf_rdd = tf_rdd.map(lambda x: (x[0], nf + (1-nf)*x[1]/max_tf))
+    return ntf_rdd
+    # Calculate the ntf-idf
+    #ntf_idf_rdd = ntf_rdd.map(lambda x: (x[0], x[1]*log(query_count/(1+int(query_freq[x[0]])))))
+    #return ntf_idf_rdd
+
+def word_count_to_ntf_DStream(word_count):
+    """ Incoming DStream of (word, count) pairs are mapped to (word, ntf) pairs,
+    where ntf = a+(1-a)*term_freq/max(term_freq). """
+    
+    nf = 0.4 # Normalization factor used when calculating normalized term frequency
+    
+    # Search for the max term frequency, max_tf, from all term frequencies, tf
+    tf = word_count.map(lambda x: x[1])
+    max_tf = tf.reduce(lambda x, y: max(x, y))
+
+    # Normalize the term frequencies
+    ntf = word_count.map(lambda x: (x[0], nf + (1-nf)*x[1]/max_tf))
+    
+    # Return the normalized term frequency
+    return ntf
+
+
 
 def sparse_norm(sparse_vector):
     vector_list = sparse_vector.collect()
@@ -68,21 +110,6 @@ def tuples_into_sparse(ind_rdd):
                                         lambda x, value: (x[0]+[value[0]], x[1]+[value[1]]),
                                         lambda x, y: (x[0]+y[0], x[1]+y[1]))
     return sparse_vec
-
-def normalize_tf(tf_rdd):
-    # Input is an RDD containing list of tuples in (term, term_freq) format
-    # Output is an RDD containing the same list as (term, a+(1-a)*term_freq/max(term_freq))
-    
-    nf = 0.4 # Normalization factor used when calculating normalized term frequency
-    # Search for the max term frequency, max_tf, from all term frequencies, tf
-    freq = tf_rdd.map(lambda x: x[1])
-    max_tf = freq.max()
-    #max_tf = max_tf.collect()[0]
-    #max_tf = tf.reduce(lambda x, y: max(x, y))
-    ntf_rdd = tf_rdd.map(lambda x: (x[0], nf + (1-nf)*x[1]/max_tf))
-    return ntf_rdd
-    #ntf_idf_rdd = ntf_rdd.map(lambda x: (x[0], x[1]*log(query_count/(1+int(query_freq[x[0]])))))
-    #return ntf_idf_rdd
 
 def cosine_similarity(rdd):
     # The format of input rdd:
@@ -185,7 +212,8 @@ if __name__ == "__main__":
     batch_duration = 10  # Batch duration (s)
     ssc = StreamingContext(sc, batch_duration)
 
-    # Broadcast lyrics to nodes
+    # Broadcast word_set, lyrics to nodes
+    word_set_bc = sc.broadcast(word_set)
     #lyrics_broadcast = sc.broadcast(lyrics_sparse)
 #    lyrics_rdd = sc.parallelize(lyrics_sparse)
 #    track_ids_broadcast = sc.broadcast(lyrics_keys_small)
@@ -205,35 +233,27 @@ if __name__ == "__main__":
 
     # Extract tweet text fields from the raw stream
     tweets = raw_stream_to_text(tweets_raw)
-    tweets.pprint()
+    #tweets.pprint()
 
-    # Map tweet text fields to collections of words
-    words = messages_to_words(tweets)
-    words.pprint()
+    # Filter from stream messages that contain theme word
+    #theme_words = ['food', 'love', 'relax', 'shop', 'sport', 'travel']
+    #tweets_w1 = tweets.filter(lambda x: theme_words[0] in x)
+    #tweets_w2 = tweets.filter(lambda x: theme_words[1] in x)
+    #tweets_w1.pprint()
+    #tweets_w2.pprint()
 
-    # Filter words not in the set of words used for bag-of-words
-#    words = words.filter(lambda x: x in word_set)
-#    words.pprint()
+    # Map tweet text fields to collections of words and their counts
+    word_count = messages_to_words(tweets)
+    word_count.pprint()
 
-    # Count the number of words in the batch of tweets
-#    pairs = words.map(lambda word: (word, 1))
-#    word_count = pairs.reduceByKey(lambda x, y: x + y)
-#    word_count.pprint()
-
-    # Search for the max term frequency, max_tf, from all term frequencies, tf
-#    tf = word_count.map(lambda x: x[1])
-#    max_tf = tf.reduce(lambda x, y: max(x, y))
-#    tf.pprint()
-#    max_tf.pprint()
+    # Calculate normalized term frequency, ntf
+    #ntf = word_count_to_ntf_DStream(word_count)
+    ntf = word_count.transform(word_count_to_ntf)
+    ntf.pprint()
 
     # Update historical query frequency and count
-#    query_count += 1
-    # query_freq OMITTED
-
-    # Normalize the word counts
-#    ntf = word_count.transform(normalize_tf)
-#    ntf.pprint()
-    
+    #query_count += 1
+    #query_freq OMITTED
 
     # Pick up n words with highest ntf-idf values (word, tf)
 #    top_words = ntf.transform(take_top_words)
