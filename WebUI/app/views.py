16b1852393daf8c35e41e74281c9e414ad2ba84e
Ubuntu <ubuntu@ip-172-31-0-237.us-west-2.compute.internal>
2016-10-07 06:31:09 +0000

    Updated WebUI files

--- WebUI/app/views.py
+++ WebUI/app/views.py
@@ -5,9 +5,9 @@ import simplejson
 
 # Set up a connection to Redis
 with open("redis_pw.json.nogit") as fh:
-    redis_pw = simplejson.loads(fh.read())
-red = redis.StrictRedis(host='172.31.0.231', port=6379, db=0,
-                        password=redis_pw["password"])
+    redis_info = simplejson.loads(fh.read())
+red = redis.StrictRedis(host=redis_info["host"], port=redis_info["port"], db=0,
+                        password=redis_info["password"])
 
 @app.route('/')
 
@@ -15,57 +15,42 @@ red = redis.StrictRedis(host='172.31.0.231', port=6379, db=0,
 def index():
     return render_template('index.html', title = 'This is VibezNow!')
 
-@app.route('/logged', methods=['POST'])
-def logged():
-    # User name entered is user_id
-    user_id = request.form["user_id"]
+@app.route('/theme', methods=['POST'])
+def theme():
+    # User  chooses "theme"
+    theme = request.form["theme"]
 
-    # Get song suggestion and trending words
-    songs = red.get('top_songs_key')
+    # Get song suggestion for the given "theme word", and trending words
+    if theme == "Food":
+        songs = red.get('top_songs_w1_key')
+        theme = "Theme: "+theme
+    elif theme == "Love":
+        songs = red.get('top_songs_w2_key')
+        theme = "Theme: "+theme
+    elif theme == "Shop":
+        songs = red.get('top_songs_w3_key')
+        theme = "Theme: "+theme
+    elif theme == "Sport":
+        songs = red.get('top_songs_w4_key')
+        theme = "Theme: "+theme
+    else:
+        songs = red.get('top_songs_key')
+        theme = "No Theme"
     songs = eval(songs)
     words = red.get('top_words_key')
     words = eval(words)
-    
-    # Pick the first song artist and title, and the list of words
-    song = eval(songs[0][0])
-    artist = song[0]
-    song = song[1]
+    song = []
+    for item in songs:
+        song.append(eval(item[0]))
+    artist_list = []
+    song_list = []
+    for item in song:
+        artist_list.append(item[0])
+        song_list.append(item[1])
     word_list = []
     for item in words:
         word_list.append(item[0])
-
+    
     # Return the top words and songs
-    jsonresponse = {"artist": artist, "song": song,  "words": word_list}
-    #print jsonresponse
-    #print type(jsonresponse)
-    #jsonresponse = jsonify(jsonresponse)
-    #print jsonresponse
-    #print simplejson.dumps(jsonresponse)
-    #print "ok"
+    jsonresponse = {"artist": artist_list, "song": song_list,  "words": word_list, "theme_word": theme}
     return render_template("user_op.html", output=[jsonresponse])
-
- 
-"""
-
-@app.route('/index')
-def index():
-    #return "Hello, World!"
-    user = { 'nickname': 'Miguel' } # fake user
-    mylist = [1, 2, 3, 4]
-    return render_template("index.html", title = 'Home', user = user, mylist = mylist)
-
-@app.route('/email')
-def email():
-    return render_template("email.html")
-
-@app.route("/email", methods=['POST'])
-def email_post():
-    emailid = request.form["emailid"]
-    date = request.form["date"]
-
-    # email entered is in emailid and date selected in dropdown is in date variable, respectively
-
-    jsonresponse = [{"fname": "x.fname", "lname": "x.lname", "id": "x.id", "message": "x.message", "time": "x.time"}]
-    return render_template("emailop.html", output=jsonresponse)
-
-"""
